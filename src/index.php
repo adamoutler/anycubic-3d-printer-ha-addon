25eb4ad9ff5b6408b4aefa96e79081c5a86d1760
Adam Outler <adamoutler@gmail.com>
2022-03-20 15:38:45 +0000

    split ui and function scripts

--- src/index.php
+++ src/index.php
@@ -127,3 +127,4 @@ print "const camera=\"".$config['MONO_X_CAMERA']."\";\n";
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="js/script.js"></script>
+<script src="js/ui.js"></script>
