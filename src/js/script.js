41814c9eb8d31a7471045237f5991ce6993d078f
Adam Outler <adamoutler@gmail.com>
2022-03-20 23:23:51 +0000

    Fix Issues with update

--- src/js/script.js
+++ src/js/script.js
@@ -3,34 +3,79 @@ selectedFileName = "";
 
 state = {};
 
-function print() {
-  document.getElementById("print").classList.add("disabled");
-  if (selectedFile != "") {
-    doApiCall("goprint," + selectedFile + ",end", function (err, data) {
-      //do stuff here.
-    });
-  }
-}
-
-function pause() {
-  document.getElementById("pause").classList.add("disabled");
-  doApiCall("gopause," + selectedFile + ",end", function (err, data) {
-    //do stuff here.
-  });
-}
-
-function stop() {
-  document.getElementById("stop").classList.add("disabled");
-  doApiCall("gostop," + selectedFile + ",end", function (err, data) {
-    //do stuff here.
-  });
-}
-
 function onSelect(item) {
   selectedFileName = item.selectedOptions[0].innerHTML;
   selectedFile = item.value;
   document.getElementById("selected").innerHTML = this.selectedFileName;
 }
+
+function removeOptions(selectElement) {
+  var i, L = selectElement.options.length - 1;
+  for(i = L; i >= 0; i--) {
+     selectElement.remove(i);
+  }
+}
+
+var callback = function handleResults(err, data) {
+  mergeState(data);
+  if (err !== null) {
+    alert("Something went wrong: " + err);
+  } else {
+    if (data == null) {
+      return;
+    }
+    if ("files" in data) {
+      removeOptions(list);
+      for (item in data.files) {
+        if (item.startsWith("end")) {
+          item = item.replace("end", "");
+          break;
+        }
+        var opt = document.createElement("option");
+        opt.value = data.files[item][1];
+        opt.innerHTML = data.files[item][0];
+        list.appendChild(opt);
+      }
+    }
+    for (var item in data) {
+      this.updateItem = null;
+      if (item == "end") {
+        continue;
+      }
+      if (item == "status" || "sysinfo") {
+
+        this.updateItem = data[item];
+        for (key in updateItem) {
+          if (key == "status") manageStates(data[item]);
+          ele = document.getElementById(key);
+          if (ele != null) ele.innerHTML = updateItem[key];
+        }
+      }
+    }
+  }
+};
+
+
+var doApiCall = function (command, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "api.php?server=" + ip + "&port=" + port + "&cmd=" + command,
+    true
+  );
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
+
 
 function action(ele) {
   var id = ele.id;
@@ -59,28 +104,40 @@ function mergeState(newstates) {
   }
 }
 
-var doApiCall = function (command, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    "api.php?server=" + ip + "&port=" + port + "&cmd=" + command,
-    true
-  );
-  xhr.responseType = "json";
-  xhr.onload = function () {
-    var status = xhr.status;
-    if (status === 200) {
-      callback(null, xhr.response);
-    } else {
-      callback(status, xhr.response);
-    }
-  };
-  xhr.send();
-};
 
-function refreshFiles() {
-  list = document.getElementById("printlist");
-  doApiCall("getfile", callback);
+
+
+
+function print() {
+  document.getElementById("print").classList.add("disabled");
+  if (selectedFile != "") {
+    doApiCall("goprint," + selectedFile + ",end", function (err, data) {
+      //do stuff here.
+    });
+  }
+}
+
+function pause() {
+  document.getElementById("pause").classList.add("disabled");
+  doApiCall("gopause," + selectedFile + ",end", function (err, data) {
+    //do stuff here.
+  });
+}
+
+function stop() {
+  document.getElementById("stop").classList.add("disabled");
+  doApiCall("gostop," + selectedFile + ",end", function (err, data) {
+    //do stuff here.
+  });
+}
+
+function enableButton(buttonName, value) {
+  ele = document.getElementById(buttonName);
+  if (value) {
+    ele.classList.remove("disabled");
+  } else {
+    ele.classList.add("disabled");
+  }
 }
 
 function enablePrint(value) {
@@ -94,16 +151,6 @@ function enablePause(value) {
 function enableStop(value) {
   enableButton("stop", value);
 }
-
-function enableButton(buttonName, value) {
-  ele = document.getElementById(buttonName);
-  if (value) {
-    ele.classList.remove("disabled");
-  } else {
-    ele.classList.add("disabled");
-  }
-}
-
 function manageStates(item) {
   if ("status" in item)
     switch (item.status) {
@@ -130,43 +177,10 @@ function manageStates(item) {
     }
 }
 
-var callback = function handleResults(err, data) {
-  mergeState(data);
-  if (err !== null) {
-    alert("Something went wrong: " + err);
-  } else {
-    if (data == null) {
-      return;
-    }
-    if ("files" in data) {
-      for (item in data.files) {
-        if (item.startsWith("end")) {
-          item = item.replace("end", "");
-          break;
-        }
-        var opt = document.createElement("option");
-        opt.value = data.files[item][1];
-        opt.innerHTML = data.files[item][0];
-        list.appendChild(opt);
-      }
-    }
-    for (var item in data) {
-      this.updateItem = null;
-      if (item == "end") {
-        continue;
-      }
-      if (item == "status"||"sysinfo") {
-        
-        this.updateItem = data[item];
-        for (key in updateItem) {
-          if (key == "status")manageStates(data[item]);
-          ele = document.getElementById(key);
-          if (ele != null) ele.innerHTML = updateItem[key];
-        }
-      }
-    }
-  }
-};
+function refreshFiles() {
+  list = document.getElementById("printlist");
+  doApiCall("getfile", callback);
+}
 
 function getStatus() {
   doApiCall("getstatus", callback);
@@ -177,34 +191,24 @@ function getSysInfo() {
 }
 
 function executeAsync(func) {
-  setTimeout(func, 0);
-}
-
-function doTenSecondRefresh() {
-  console.log("status");
-  getStatus();
-  sleep(3000);
-  refreshFiles();
-  sleep(3000);
-  getSysInfo();
-  sleep(3000);
-
-  setTimeout(doTenSecondRefresh, 15000);
+  setTimeout(func);
 }
 
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function doSlowInitial() {
+async function doTenSecondRefresh() {
   refreshFiles();
-  sleep(3000);
-  getSysInfo();
-
-  sleep(3000);
+  await sleep(1000);
   getStatus();
-  sleep(3000);
-  
-  executeAsync(doTenSecondRefresh());
+  await sleep(1000);
+  getSysInfo();
+  await sleep(1000);
+
+  executeAsync("doTenSecondRefresh(), 15000");
 }
-doSlowInitial();
+
+
+
+executeAsync(doTenSecondRefresh);
