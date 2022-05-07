0a9a6fb93ff838eae1cb1b1148ccb7614053c841
Adam Outler <adamoutler@gmail.com>
2022-05-07 17:42:28 +0000

    use a default x instead of null to prevent def.

--- docker.entrypoint.d.init_variables.sh
+++ docker.entrypoint.d.init_variables.sh
@@ -1,18 +1,19 @@
+#Define out output file. this is where the app gets information from. 
 out="/app/config.inc.php";
-
 echo '<?php'>${out};
 
-if [ -z "${MONO_X_IP}" ]; then
+#test each variable and write an output to the output file
+if [ -z "${MONO_X_IP+x}" ]; then
     echo "\$config['MONO_X_IP'] = '192.168.1.254';" >>"${out}"
 else
     echo "\$config['MONO_X_IP'] = '${MONO_X_IP}';" >>"${out}"
 fi
-if [ -z "${MONO_X_PORT}" ]; then
+if [ -z "${MONO_X_PORT+x}" ]; then
     echo "\$config['MONO_X_PORT'] = '6000';" >>"${out}"
 else
     echo "\$config['MONO_X_PORT'] = '${MONO_X_PORT}';" >>"${out}"
 fi
-if [ -z "${MONO_X_CAMERA}" ]; then
+if [ -z "${MONO_X_CAMERA+x}" ]; then
     echo "\$config['MONO_X_CAMERA'] = '';" >>${out}
 else
     echo "\$config['MONO_X_CAMERA'] = '${MONO_X_CAMERA}';" >>${out}
