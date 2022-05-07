946f66ea2d126c2155a2fc85934ad0a92a2e74c5
Adam Outler <adamoutler@gmail.com>
2022-05-07 18:05:46 +0000

    Add quotes

--- docker.entrypoint.d.init_variables.sh
+++ docker.entrypoint.d.init_variables.sh
@@ -6,15 +6,15 @@ echo '<?php'>${out};
 if [ -z "${MONO_X_IP+x}" ]; then
     echo "\$config['MONO_X_IP'] = '192.168.1.254';" >>"${out}"
 else
-    echo "\$config['MONO_X_IP'] = ${MONO_X_IP};" >>"${out}"
+    echo "\$config['MONO_X_IP'] = '${MONO_X_IP}';" >>"${out}"
 fi
 if [ -z "${MONO_X_PORT+x}" ]; then
     echo "\$config['MONO_X_PORT'] = '6000';" >>"${out}"
 else
-    echo "\$config['MONO_X_PORT'] = ${MONO_X_PORT};" >>"${out}"
+    echo "\$config['MONO_X_PORT'] = '${MONO_X_PORT}';" >>"${out}"
 fi
 if [ -z "${MONO_X_CAMERA+x}" ]; then
     echo "\$config['MONO_X_CAMERA'] = '';" >>${out}
 else
-    echo "\$config['MONO_X_CAMERA'] = ${MONO_X_CAMERA};" >>${out}
+    echo "\$config['MONO_X_CAMERA'] = '${MONO_X_CAMERA}';" >>${out}
 fi
