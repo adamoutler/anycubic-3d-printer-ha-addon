5dd4001aa15cdfb21bdfb8134ee1642eda05502e
Adam Outler <adamoutler@gmail.com>
2022-05-07 17:33:36 +0000

    attempt to fix shellcheck

--- docker.entrypoint.d.init_variables.sh
+++ docker.entrypoint.d.init_variables.sh
@@ -2,18 +2,18 @@ out="/app/config.inc.php";
 
 echo '<?php'>${out};
 
-if [ -z ${MONO_X_IP} ]; then
-    echo "\$config['MONO_X_IP'] = '192.168.1.254';" >>${out}
+if [ -z "${MONO_X_IP}" ]; then
+    echo "\$config['MONO_X_IP'] = '192.168.1.254';" >>"${out}"
 else
-    echo "\$config['MONO_X_IP'] = '"${MONO_X_IP}"';" >>${out}
+    echo "\$config['MONO_X_IP'] = '${MONO_X_IP}';" >>"${out}"
 fi
-if [ -z ${MONO_X_PORT} ]; then
-    echo "\$config['MONO_X_PORT'] = '6000';" >>${out}
+if [ -z "${MONO_X_PORT}" ]; then
+    echo "\$config['MONO_X_PORT'] = '6000';" >>"${out}"
 else
-    echo "\$config['MONO_X_PORT'] = '"${MONO_X_PORT}"';" >>${out}
+    echo "\$config['MONO_X_PORT'] = '${MONO_X_PORT}';" >>"${out}"
 fi
-if [ -z ${MONO_X_CAMERA} ]; then
+if [ -z "${MONO_X_CAMERA}" ]; then
     echo "\$config['MONO_X_CAMERA'] = '';" >>${out}
 else
-    echo "\$config['MONO_X_CAMERA'] = '"${MONO_X_CAMERA}"';" >>${out}
+    echo "\$config['MONO_X_CAMERA'] = '${MONO_X_CAMERA}';" >>${out}
 fi
