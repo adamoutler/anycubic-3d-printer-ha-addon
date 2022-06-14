340e3a6269bb5642f09db9b989737ca7bcc8a8d8
Adam Outler <adamoutler@gmail.com>
2022-06-14 01:19:35 +0000

    Update UI to remove floating windows

--- src/index.php
+++ src/index.php
@@ -32,71 +32,87 @@
         </span>
 
     </div>
-    <div id='window1' class='right window'>
-        <div id="box1" class='title'>camera feed</div>
-        <img src=http://192.168.1.251:8080/?action=stream&1647305721063 alt="Your browser may be blocking insecure content on a secure page.  You can allow insecure content, or disable the camera." />
-
-    </div>
-    <div id='window2' class='top left window'>
+    <div class="block">
         <div id="box2" class='title'><span>status: <span id="status"></span> <span id="file"></span> <span id="total_volume"></span></span></div>
-        <div class='justify-content-between'>
-            <div><span>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" id="progress-bar"><span id="percent_complete"></span></progress></div>
-                </span>
+        <div><span>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" id="progress-bar"><span id="percent_complete"></span></progress></div>
+            </span>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <button type="button" onclick="action(this)" id='print' class="btn btn-success">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
+                        <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z">
+                        </path>
+                    </svg>
+                </button>
+                <button type="button" id='pause' onclick="action(this)" class="btn btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">
+                        <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z">
+                        </path>
+                    </svg>
+                </button>
+                <button type="button" id='stop' onclick="action(this)" class="btn btn-danger">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop" viewBox="0 0 16 16">
+                        <path d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z">
+                        </path>
+                    </svg>
+                </button>
+                <div>
+                    <span id='selected'></span>
+                </div>
+                <div>
+                    <select id="printlist" name="toprint" onchange="onSelect(this);" size="10">
+                    </select>
+                </div>
             </div>
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">layers</th>
-                        <th scope="col">layer height</th>
-                        <th scope="col">remaining</th>
-                        <th scope="col">elapsed</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><span><span id="current_layer"></span>/<span id="total_layers"></span></span> </td>
-                        <td id="layer_height"></td>
-                        <td id="seconds_remaining"></td>
-                        <td id="elapsed"></td>
-                    </tr>
-                <tbody>
-            </table>
 
+            <div class="col-md-4 text-center">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">layers</th>
+                            <td> <span><span id="current_layer"></span>/<span id="total_layers"></span></span> </td>
 
+                        </tr>
+                        <tr>
+                            <th scope="col">layer height</th>
+                            <td id="layer_height"></td>
+
+                        </tr>
+                        <tr>
+
+                            <th scope="col">remaining</th>
+                            <td id="seconds_remaining"></td>
+
+                        </tr>
+                        <tr>
+
+                            <th scope="col">elapsed</th>
+                            <td id="elapsed"></td>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+
+                        </tr>
+                    <tbody>
+                </table>
+            </div>
         </div>
     </div>
-    <div id='window3' class='left window'>
-        <div id="box3" class='title'>controls</div>
-        <button type="button" onclick="action(this)" id='print' class="btn btn-success">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
-                <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z">
-                </path>
-            </svg>
-        </button>
-        <button type="button" id='pause' onclick="action(this)" class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">
-                <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z">
-                </path>
-            </svg>
-        </button>
-        <button type="button" id='stop' onclick="action(this)" class="btn btn-danger">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop" viewBox="0 0 16 16">
-                <path d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z">
-                </path>
-            </svg>
-        </button>
-        <div>
-            <span id='selected'></span>
-        </div>
-        <div>
-            <select id="printlist" name="toprint" onchange="onSelect(this);" size="10">
-            </select>
-        </div>
+    
+
+
+
+    <div id='window1' class='window'>
+        <div id="box1" class='title'>camera feed</div>
+        <img id="camerabox" src=http://192.168.1.251:8080/?action=stream&1647305721063 alt="Your browser may be blocking insecure content on a secure page.  You can allow insecure content, or disable the camera." />
+
     </div>
-
-
+    </div>
 </body>
 
 <script>
@@ -112,6 +128,7 @@
     print "const camera=\"" . $config['MONO_X_CAMERA'] . "\";\n";
 
     ?>
+    document.getElementById("camerabox").setAttribute("src", camera);
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
