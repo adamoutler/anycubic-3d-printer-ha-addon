77417cd547e8317e68591ed88acdafd831c1b267
Adam Outler <adamoutler@gmail.com>
2022-06-19 18:16:15 +0000

    adjust monox docker entry conatiner variable

--- docker.entrypoint.d.init_variables.sh
+++ docker.entrypoint.d.init_variables.sh
@@ -14,7 +14,7 @@ else
     echo "\$config['MONO_X_PORT'] = '${MONO_X_PORT}';" >>"${out}"
 fi
 if [ -z "${MONO_X_CAMERA+x}" ]; then
-    echo "\$config['MONO_X_CAMERA'] = '';" >>${out}
+    echo "\$config['MONO_X_USE_CAMERA'] = '';" >>${out}
 else
-    echo "\$config['MONO_X_CAMERA'] = '${MONO_X_CAMERA}';" >>${out}
+    echo "\$config['MONO_X_USE_CAMERA'] = '${MONO_X_CAMERA}';" >>${out}
 fi
