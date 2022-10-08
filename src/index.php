e03ed44858717377322cda3a45a96dd324efb6a5
Adam Outler <adamoutler@gmail.com>
2022-10-08 21:28:49 +0000

    Remove unused div

--- src/index.php
+++ src/index.php
@@ -7,20 +7,6 @@
 </head>
 
 <body>
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-        <div class="toast-header">
-            <svg class="rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-                <rect fill="#007aff" width="100%" height="100%" />
-            </svg>
-            <strong class="mr-auto">Bootstrap</strong>
-            <small class="text-muted">just now</small>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="toast-body">
-        </div>
-    </div>
     <div id="header" class="position-static heading-box">
         <row class="d-inline-flex">
             <img class="header-image" src="img/anycubic.jpg" />
